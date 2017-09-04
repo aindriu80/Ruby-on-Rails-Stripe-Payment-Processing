6a5bd8e358eb1cf5e7c0eab17b9e1eb6d6d28bb8
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2017-09-04 10:58:36 +0100

    email receipt to customer

---
+++ app/mailers/purchase_mailer.rb
@@ -0,0 +1,10 @@
+class PurchaseMailer < ActionMailer::Base
+    layout 'purchase_mailer'
+    default from: "Rails by Aindriu <aindriu80@gamil.com>"
+    
+    def purchase_receipt purchase
+        @purchase = purchase
+        mail to: purchase.email, subject: "Thanks for your business!"
+    end
+    
+end
